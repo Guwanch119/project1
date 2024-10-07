645c20e72f03bf99f99aff146126af3af6654a6c
rustem <ragamyradow22@gmail.com>
2024-10-07 16:44:52 +0500

    catylan

--- project_management/project/urls.py
+++ project_management/project/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('',views.homeView,name="homeURL"),
+    path('bizbarada',views.bizbaradaView,name="bizbaradaURL"),
+    path('yasalymlar',views.yasalymlarView,name="yasalymlarURL"),
+    path('yumuslar',views.yumuslarView,name="yumuslarURL"),
+    path('statistika',views.statistikaView,name="statistikaURL"),
+    path('kalendar',views.kalendarView,name="kalendarURL"),
 ]
--- project_management/project/views.py
+++ project_management/project/views.py
@@ -3,3 +3,20 @@ from django.shortcuts import render
 
 def homeView(request):
     return render(request,'index.html')
+
+
+
+def bizbaradaView(request):
+    return render(request,'bizbarada.html')
+
+def yasalymlarView(request):
+    return render(request,'yasalymlar.html')
+
+def yumuslarView(request):
+    return render(request,'yumuslar.html')
+
+def statistikaView(request):
+    return render(request,'statistika.html')
+
+def kalendarView(request):
+    return render(request,'kalendar.html')
